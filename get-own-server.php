6374db1f6c0f2a20e4373f4d1178e205e6c8167f
mikemarom <mike.marom@gmail.com>
2023-08-05 10:18:11 -0700

    Split client code

---
+++ get-own-server.php
@@ -0,0 +1,26 @@
+<?php
+// Initialize cURL session
+$curl_connection=curl_init();
+
+// Set up endpoint
+curl_setopt($curl_connection,CURLOPT_URL, 'https://marom.dev/rest-api-server/account/2345');
+
+// Return the response of the cURL command as a string
+curl_setopt($curl_connection,CURLOPT_RETURNTRANSFER, true);
+
+// Execute the cURL call
+$response = curl_exec($curl_connection);
+
+// Close the cURL connection
+curl_close($curl_connection);
+
+echo '<p>Response String is: '.$response.'</p>';
+
+// Parse the JSON string response into an array
+$jsonArrayResponse = json_decode($response);
+
+echo '<p>Response Array is: <br><pre>';
+echo var_dump($jsonArrayResponse);
+echo '</pre></p>';
+
+?>
